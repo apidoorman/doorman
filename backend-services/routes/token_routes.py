b07ea8b050762a5ab77701a9c49d54bc481810c3
pyperson <pyperson@users.noreply.github.com>
2025-09-19 22:31:58 -0400

    pre release updates

--- backend-services/routes/token_routes.py
+++ backend-services/routes/token_routes.py
@@ -24,6 +24,71 @@ token_router = APIRouter()
 
 logger = logging.getLogger("doorman.gateway")
 
+@token_router.get("/defs",
+    description="List token definitions",
+    response_model=ResponseModel,
+    responses={
+        200: {"description": "Successful Response"}
+    }
+)
+async def list_token_definitions(request: Request, page: int = 1, page_size: int = 50):
+    request_id = str(uuid.uuid4())
+    start_time = time.time() * 1000
+    try:
+        payload = await auth_required(request)
+        username = payload.get("sub")
+        logger.info(f"{request_id} | Username: {username} | From: {request.client.host}:{request.client.port}")
+        logger.info(f"{request_id} | Endpoint: {request.method} {str(request.url.path)}")
+        if not await platform_role_required_bool(username, 'manage_tokens'):
+            return respond_rest(ResponseModel(
+                status_code=403,
+                error_code='TKN002',
+                error_message='Unable to retrieve tokens'
+            ))
+        return respond_rest(await TokenService.list_token_defs(page, page_size, request_id))
+    except Exception as e:
+        logger.critical(f"{request_id} | Unexpected error: {str(e)}", exc_info=True)
+        return process_response(ResponseModel(
+            status_code=500,
+            response_headers={"request_id": request_id},
+            error_code="GTW999",
+            error_message="An unexpected error occurred"
+        ).dict(), "rest")
+    finally:
+        end_time = time.time() * 1000
+        logger.info(f"{request_id} | Total time: {str(end_time - start_time)}ms")
+
+@token_router.get("/defs/{api_token_group}",
+    description="Get a token definition",
+    response_model=ResponseModel,
+)
+async def get_token_definition(api_token_group: str, request: Request):
+    request_id = str(uuid.uuid4())
+    start_time = time.time() * 1000
+    try:
+        payload = await auth_required(request)
+        username = payload.get("sub")
+        logger.info(f"{request_id} | Username: {username} | From: {request.client.host}:{request.client.port}")
+        logger.info(f"{request_id} | Endpoint: {request.method} {str(request.url.path)}")
+        if not await platform_role_required_bool(username, 'manage_tokens'):
+            return respond_rest(ResponseModel(
+                status_code=403,
+                error_code='TKN002',
+                error_message='Unable to retrieve tokens'
+            ))
+        return respond_rest(await TokenService.get_token_def(api_token_group, request_id))
+    except Exception as e:
+        logger.critical(f"{request_id} | Unexpected error: {str(e)}", exc_info=True)
+        return process_response(ResponseModel(
+            status_code=500,
+            response_headers={"request_id": request_id},
+            error_code="GTW999",
+            error_message="An unexpected error occurred"
+        ).dict(), "rest")
+    finally:
+        end_time = time.time() * 1000
+        logger.info(f"{request_id} | Total time: {str(end_time - start_time)}ms")
+
 @token_router.post("",
     description="Create a token",
     response_model=ResponseModel,
